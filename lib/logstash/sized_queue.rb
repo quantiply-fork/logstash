6ebd42fd01f046df2f078bba062d4251177583d1
Sumit Arrawatia <arrawatia@sumits-mbp-2.etrade.com>
2014-11-14 11:32:01 -0800

    More metrics.YAY

--- lib/logstash/sized_queue.rb
+++ lib/logstash/sized_queue.rb
@@ -14,15 +14,32 @@ class LogStash::SizedQueue < SizedQueue
         LogStash.metrics_registry.register("#{name}.queue.size", QueueMetric.new(self))
         @push_rate = LogStash.metrics_registry.meter("#{name}.push.rate")
         @pop_rate = LogStash.metrics_registry.meter("#{name}.pop.rate")
+        @tracer_metrics = {}
+        @count_metrics = {}
     end
 
     def push(event)
+        @push_rate.mark
+        if event[:tracer] 
+            metric_name = "#{name}.queue.in.#{event[:tracer][:name]}"
+            c_metric = @tracer_metrics[metric_name] || LogStash.metrics_registry.counter("#{metric_name}.count")
+            t_metric = @tracer_metrics[metric_name] || LogStash.metrics_registry.register("#{metric_name}.trace", TracerMetric.new(0))
+            t_metric.update(event[:tracer][:count])
+            c_metric.inc
+        end
         @push_rate.mark
         super(event)
     end
 
     def pop
         @pop_rate.mark
+        if event[:tracer] 
+            metric_name = "#{name}.queue.out.#{event[:tracer][:name]}"
+            c_metric = @tracer_metrics[metric_name] || LogStash.metrics_registry.counter("#{metric_name}.count")
+            metric = @tracer_metrics[metric_name] || LogStash.metrics_registry.register(metric_name, TracerMetric.new(0))
+            metric.update(event[:tracer][:count])
+            c_metric.inc
+        end
         super
     end
 
@@ -38,7 +55,25 @@ class QueueMetric
 
     # java_signature 'int getValue()'
     def getValue
-        q.length
+        @q.length
+    end
+end
+
+class TracerMetric
+    # java_implements 'com.codahale.metrics.Gauge'
+    include com.codahale.metrics.Gauge
+    
+    def initialize(q)
+        @q = q
+    end
+
+    def update(u)
+        @q = u
+    end
+
+    # java_signature 'int getValue()'
+    def getValue
+        @q
     end
 end
 
